2f77c4614c7f90500994982adc29972a5a83c2c9
admin <wangjunhui@nequal.com>
2019-05-09 17:43:33 +0800

    delete

--- server/public/build/static/js/24.8b38b3d4.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{317:function(n,w,o){}}]);
-//# sourceMappingURL=24.8b38b3d4.chunk.js.map
--- server/public/build/static/js/25.55bac5eb.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{316:function(n,w,o){}}]);
-//# sourceMappingURL=25.55bac5eb.chunk.js.map
