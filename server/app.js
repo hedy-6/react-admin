d0fcaa80fcaf9b41cbe4611f917a267d3c0a8ebb
admin <wangjunhui@nequal.com>
2019-05-09 18:19:55 +0800

    build

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/', indexRouter);
 app.use(`${baseUrl}/users`, usersRouter);
