743d40a23d9f8fdc8d8a65ca3d69155b72c6cf12
admin <wangjunhui@nequal.com>
2019-05-09 17:11:44 +0800

    server 修改

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.use('/', indexRouter);
 app.use(`${baseUrl}/users`, usersRouter);
