b2616b3658d437a6430335862d4ef6eb2f79c8d3
admin <wangjunhui@nequal.com>
2019-05-09 17:51:04 +0800

    server static 配置

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/', indexRouter);
 app.use(`${baseUrl}/users`, usersRouter);
